d89a7d655c8d4f851e4b3ecf941d97132046a1e5
faorec <111794705+faorec@users.noreply.github.com>
2022-10-21 17:04:04 -0600

    Add files via upload
    actividades correspondientes a la semana 11

---
+++ 11.0.0/L12_FAOC_1024822/L2_FAOC_1024822/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SEGUNDO EJEMPLO VECTORES CON FUNCIONES!");
+        pruebaVector pv = new pruebaVector();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+
+    class pruebaVector
+    {
+        private int[] notas;
+        public int suma = 0;
+        public void Cargar()
+        {
+            notas = new int[15];
+            for (int f = 0; f < 15; f++)
+            {
+                Console.WriteLine("Ingresa su nota: ");
+                string linea;
+                linea = Console.ReadLine();
+                notas[f] = int.Parse(linea);
+                suma = suma + notas[f];
+            }
+        }
+        public void Imprimir()
+        {
+            for (int f = 0; f < 15; f++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Se ingreso ");
+                Console.WriteLine(notas[f]);
+            }
+            Console.WriteLine("La suma es : " + suma);
+            Console.WriteLine("El promedio es : " + suma /15);
+
+            Console.ReadKey();
+
+            
+        }
+
+    }
+}
+
---
+++ 11.0.0/L13_FAOC_1024822/L13_FAOC_1024822/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" ");
+        int[] edad = new int [5];
+        string[] nombre = new string[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Ingrese Nombre de persona: ");
+            nombre[i] =Console.ReadLine();
+
+            Console.WriteLine("Ingrese Edad de la persona : ");
+            edad[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Los mayores de edad y sus edades son:");
+
+        for ( int i =0; i < 5; i++)
+        {
+            if( edad[i] >= 18)
+            {
+                Console.WriteLine(nombre[i] + ", " + edad[i]);
+               
+            }
+
+
+        }
+
+        Console.ReadKey();
+    }
+    
+
+    
+    
+}
+
