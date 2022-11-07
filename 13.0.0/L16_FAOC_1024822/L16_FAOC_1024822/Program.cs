f68f5150d09bad7215ca78b295ffcc26d3469a35
faorec <111794705+faorec@users.noreply.github.com>
2022-11-07 12:44:02 -0600

    Add files via upload
    actividades correspondientes a la semana 13

---
+++ 13.0.0/L16_FAOC_1024822/L16_FAOC_1024822/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+
+    public static int[,] operacion = new int[40, 50];
+
+    static void Main(string[] args)
+    {
+
+        int suma1 = 0;
+        int suma2 = 0;
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < 40; i++)
+        {
+            for (int x = 0; x < 50; x++)
+            {
+                operacion[i, x] = rnd.Next(-40, 50);
+
+            }
+        }
+
+        for (int i = 0; i < 40; i++)
+        {
+            for (int x = 0; x < 50; x++)
+            {
+                if (operacion[i, x] < 0)
+                {
+                    
+                    suma2 = suma2 + operacion[i, x];
+                    
+                }
+            }
+        }
+
+        for (int i = 0; i < 40; i++)
+        {
+            for (int x = 0; x < 50; x++)
+            {
+                if (operacion[i, x] > 0)
+                {
+                    
+                    suma1 = suma1 + operacion[i, x];
+                     
+                }
+            }
+        }
+
+        Console.WriteLine(suma1);
+        Console.WriteLine(suma2);
+        Console.ReadKey();
+    } 
+}
+
+
+
